800c6670561e91cbf4e9986abd41c8f039cee9ec
Manuela Arcila Cruz <manu-142000@hotmail.com>
2021-04-12 16:59:51 -0500

    Agregar archivos de proyecto.

---
+++ Constructora/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Constructora.MvcApplication" Language="C#" %>
---
+++ ConstructoraModel/DbModel/RoleDbModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructoraModel.DbModel
+{
+    public class RoleDbModel
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private bool removed;
+
+        public bool Removed
+        {
+            get { return removed; }
+            set { removed = value; }
+        }
+
+
+
+    }
+}
